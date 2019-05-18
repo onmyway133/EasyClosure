983b593194c608336bcba2317d77be5bd3e86a66
Khoa Pham <onmyway133@gmail.com>
2019-05-18 22:16:53 +0200

    Update demo

--- Example/EasyClosureDemo/EasyClosureDemo/Sources/ViewController.swift
+++ Example/EasyClosureDemo/EasyClosureDemo/Sources/ViewController.swift
@@ -3,37 +3,37 @@ import EasyClosure
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var goodSwitch: UISwitch!
-  @IBOutlet weak var cheapSwitch: UISwitch!
-  @IBOutlet weak var fastSwitch: UISwitch!
+    @IBOutlet weak var goodSwitch: UISwitch!
+    @IBOutlet weak var cheapSwitch: UISwitch!
+    @IBOutlet weak var fastSwitch: UISwitch!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    let good = self.goodSwitch!
-    let cheap = self.cheapSwitch!
-    let fast = self.fastSwitch!
+        let good = self.goodSwitch!
+        let cheap = self.cheapSwitch!
+        let fast = self.fastSwitch!
 
-    func allEasyClosure() -> Bool {
-      return [good, cheap, fast].filter({ $0.isEasyClosure }).count == 3
+        func allEasyClosure() -> Bool {
+            return [good, cheap, fast].filter({ $0.isOn }).count == 3
+        }
+
+        good.on.valueChanged {
+            if allEasyClosure() {
+                fast.setOn(false, animated: true)
+            }
+        }
+
+        cheap.on.valueChanged {
+            if allEasyClosure() {
+                good.setOn(false, animated: true)
+            }
+        }
+
+        fast.on.valueChanged {
+            if allEasyClosure() {
+                cheap.setOn(false, animated: true)
+            }
+        }
     }
-
-    good.on.change {
-      if allEasyClosure() {
-        fast.setEasyClosure(false, animated: true)
-      }
-    }
-
-    cheap.on.change {
-      if allEasyClosure() {
-        good.setEasyClosure(false, animated: true)
-      }
-    }
-
-    fast.on.change {
-      if allEasyClosure() {
-        cheap.setEasyClosure(false, animated: true)
-      }
-    }
-  }
 }
